45211c9d6e7cdc95a41b9be92c19149a694250e3
aj_barber <aj_barber@yahoo.com.au>
2017-05-22 20:31:21 +1000

    render the assimp model without transform

---
+++ src/herodrawable.cpp
@@ -0,0 +1,7 @@
+#include "herodrawable.h"
+
+HeroDrawable::HeroDrawable()
+{
+
+}
+
---
+++ src/herodrawable.h
@@ -0,0 +1,11 @@
+#ifndef HERODRAWABLE_H
+#define HERODRAWABLE_H
+
+
+class HeroDrawable
+{
+public:
+    HeroDrawable();
+};
+
+#endif // HERODRAWABLE_H
